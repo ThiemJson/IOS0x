b5abbcf315d0f463a0ead11a9c352a495038d88d
Nguyen Cao Thiem <nguyen_thiem@teneocto.io>
2020-12-20 20:16:21 +0700

    Initial Commit

---
+++ Expense/ExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseApp.swift
+//  Expense
+//
+//  Created by Thiem Jason on 12/20/20.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
